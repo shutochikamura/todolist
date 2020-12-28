1dd87bd610963f6e94c3b08dc61b810c9631b91c
shutochikamura <scmarudekaze@gmail.com>
2020-12-28 13:24:07 +0000

    修正

--- ToDoList/routes/web.php
+++ ToDoList/routes/web.php
@@ -20,4 +20,3 @@ Route::get('index', 'App\Http\Controllers\TodoController@index');
 Route::post('index/create', 'App\Http\Controllers\TodoController@add');
 Route::post('index/delete/{id}', 'App\Http\Controllers\TodoController@remove');
 Route::post('index/state/{id}', 'App\Http\Controllers\TodoController@state');
-Route::post('index', 'App\Http\Controllers\TodoController@formSwitch');
